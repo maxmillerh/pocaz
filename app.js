4379d2444e526764e5755c7e311a3e771f6c90b6
maxmillerh <85723044+maxmillerh@users.noreply.github.com>
2022-06-11 17:23:04 +0300

    12

---
+++ app.js
@@ -0,0 +1,69 @@
+var iz = 20;
+var monschet = 0;
+var dobav = 1;
+var lvl = 1;
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var count = 0;
+var foto = document.getElementById("fotoGus");
+var disp = document.getElementById("displayText");
+var tebos = document.getElementById("textBoss");
+var mon = document.getElementById("money");
+
+foto.onclick = function () {
+    count++;
+    disp.innerHTML = 'Ударов ' + count + ' из ' + iz;
+    monschet = monschet + dobav;
+    mon.innerHTML ='Денег: ' + monschet;
+
+    if(count < 20 && lvl == 1){
+        iz = 20;
+        disp.innerHTML = 'Ударов ' + count + ' из ' + iz;
+        lvl = arr[2];
+} else if(count == 20 && lvl == 2){
+        foto.src = 'img/dub.png';
+        count = 0;
+        iz = 50;
+        disp.innerHTML = 'Ударов ' + count + ' из ' + iz;
+        dobav = 2;
+        monschet = monschet + 50;
+        mon.innerHTML ='Денег: ' + monschet;
+        lvl = arr[3];
+} else if(count == 50 && lvl == 3){
+        foto.src = 'img/ananas.png';
+        count = 0;
+        iz = 100;
+        disp.innerHTML = 'Ударов ' + count + ' из ' + iz;
+        dobav = 3;
+        monschet = monschet + 100;
+        mon.innerHTML ='Денег: ' + monschet;
+        lvl = arr[4];
+} else if(count == 100 && lvl == 4){
+        foto.src = 'img/burat.png';
+        count = 0;
+        iz = 250;
+        disp.innerHTML = 'Ударов ' + count + ' из ' + iz;
+        dobav = 4;
+        monschet = monschet + 300;
+        mon.innerHTML ='Денег: ' + monschet;
+        lvl = arr[5];
+} else if(count == 250 && lvl == 5){
+        foto.src = 'img/boss.png';
+        tebos.innerHTML = 'БОСС!!';
+        count = 0;
+        iz = 500;
+        disp.innerHTML = 'Ударов ' + count + ' из ' + iz;
+        dobav = 5;
+        monschet = monschet + 550;
+        mon.innerHTML ='Денег: ' + monschet;
+        lvl = arr[6];
+} else if(count == 500 && lvl == 6){
+        disp.innerHTML = 'Ударов ' + count + ' из ' + iz;
+        monschet = monschet + 1200;
+        mon.innerHTML ='Денег: ' + monschet;
+        alert("ПОБЕДА!!!")
+
+};
+
+
+
+};
